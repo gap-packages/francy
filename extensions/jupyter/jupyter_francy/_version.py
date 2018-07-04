a585804753e85b1758507b48e02f232ec43e950d
Manuel Martins <manuelmartins25@gmail.com>
2018-07-04 09:04:32 +0000

    well

--- extensions/jupyter/jupyter_francy/_version.py
+++ extensions/jupyter/jupyter_francy/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Manuel Martins.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 6, 0, 'beta')
+version_info = (0, 8, 0, 'beta')
 __version__ = ".".join(map(str, version_info))
