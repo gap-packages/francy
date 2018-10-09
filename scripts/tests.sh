5bdfa88417e0e05ce161a554633c3dc122ce8ca1
Manuel Martins <manuelmartins25@gmail.com>
2018-10-09 22:14:17 +0000

    version increase

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -48,3 +48,36 @@ npm run docs
 cd $CURRENT/gap
 
 cat makedoc.g | $GAP
+
+
+
+
+
+# Release documentation
+
+cd $CURRENT/gap
+
+# configure git
+git config credential.helper "store --file=$CURRENT/.git-credentials"
+echo "https://${GITHUB_ADMIN_KEY}:@github.com" > $CURRENT/.git-credentials
+
+# Add a new remote pointing to the GitHubPagesForGAP repository
+git remote add gh-gap https://github.com/gap-system/GitHubPagesForGAP
+git fetch gh-gap
+
+# Create a fresh gh-pages branch from the new remote
+git branch gh-pages gh-gap/gh-pages --no-track
+
+# Create a new worktree and change into it
+git worktree add gh-pages gh-pages
+cd gh-pages
+
+cp -f ../PackageInfo.g ../README* .
+cp -f ../doc/*.{css,html,js,txt} doc/
+mkdir -p doc/js && cp -rf ../../js/doc doc/js
+
+$GAP update.g
+
+git add PackageInfo.g README* doc/ _data/package.yml
+git commit -m "Setup gh-pages based on GitHubPagesForGAP"
+git push -f --set-upstream origin gh-pages
